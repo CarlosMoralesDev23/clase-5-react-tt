0841388b8e1f426b7aa437d27f7f4d2aaee0740e
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2025-05-18 09:29:57 -0300

    Se instalo Not-Found, Icon-eliminar, useEffect

--- src/Components/Cart/SubtotalAndDeletProduct.jsx
+++ src/Components/Cart/SubtotalAndDeletProduct.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import "../../Styles/CartStyles/SubtotalAndDeletProduct.css"
+import "../../Styles/CartStyles/SubtotalAndDeletProduct.css";
+import Basura from "../../assets/basura.png";
+
 
 const SubtotalAndDeletProduct = ( { product, cart, removeProduct  } ) => {
     return (
         <div className="SubtotalAndDeletProduct">
             <span>SubTotal: ${product.subtotal}</span>
 
-            <button onClick={() => removeProduct(product)}>Eliminar</button>
+            <button onClick={() => removeProduct(product)}>
+                <img src={Basura}/>
+            </button>
         </div>
     );
 };
 
 export default SubtotalAndDeletProduct;
+
+
+
+
+
--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,12 +1,13 @@
 import Header from "../Components/Header";
 import Nav from "../Components/Nav";
 import Main from "../Components/Main";
-import Cart from "../Components/Cart/Cart"
+import Cart from "../Components/Cart/Cart";
 import ListProducts from "../Components/ListProducts";
 import Footer from "../Components/Footer";
 import { products } from "../Utils/data.js";
-import {useState, useEffect} from "react";
-
+import { useState, useEffect } from "react";
+import Loader from "../assets/Loader.gif";
+import NF404NF from "../assets/NF404NF.gif"
 
 const Home = ({
     cart,
@@ -16,20 +17,27 @@ const Home = ({
     removeProduct,
     emptyCart,
 }) => {
+    const [productos, setProductos] = useState([]);
+    const [carga, setCarga] = useState(true);
+    const [error, setError] = useState(false)
 
-    const [ productos , setProductos ] = useState([])
-
-    useEffect(()=>{
-
-
+    useEffect(() => {
         fetch(
             "https://68293f096075e87073a609b7.mockapi.io/productos-ecommerce/productos"
         )
             .then((respuesta) => respuesta.json())
-            .then((datos) => setProductos(datos))
-            .catch((error) => console.log("Error: ", error));
+            .then((datos) => {
+                setProductos(datos), setCarga(false);
+            })
+            .catch((error) => {
+                console.log("Error: ", error), setCarga(false), setError(true);
+            });
+    }, []);
 
-    }, [] )
+    if(error){
+        return <img src={NF404NF} alt="Loader" />;
+
+    }
 
     return (
         <>
@@ -44,8 +52,12 @@ const Home = ({
                 removeProduct={removeProduct}
                 emptyCart={emptyCart}
             />
-            
-            <ListProducts addToCart={addToCart} products={productos} />
+
+            {carga ? (
+                <img src={Loader} alt="Loader" />
+            ) : (
+                <ListProducts addToCart={addToCart} products={productos} />
+            )}
 
             <Footer />
         </>
