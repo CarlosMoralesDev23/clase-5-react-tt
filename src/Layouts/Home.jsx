21fc11c733b9a1c12ec15b064054a86685f81a85
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2025-05-18 08:17:01 -0300

    funcionando bien con el useEffect

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -5,6 +5,7 @@ import Cart from "../Components/Cart/Cart"
 import ListProducts from "../Components/ListProducts";
 import Footer from "../Components/Footer";
 import { products } from "../Utils/data.js";
+import {useState, useEffect} from "react";
 
 
 const Home = ({
@@ -15,6 +16,21 @@ const Home = ({
     removeProduct,
     emptyCart,
 }) => {
+
+    const [ productos , setProductos ] = useState([])
+
+    useEffect(()=>{
+
+
+        fetch(
+            "https://68293f096075e87073a609b7.mockapi.io/productos-ecommerce/productos"
+        )
+            .then((respuesta) => respuesta.json())
+            .then((datos) => setProductos(datos))
+            .catch((error) => console.log("Error: ", error));
+
+    }, [] )
+
     return (
         <>
             <Header />
@@ -29,7 +45,7 @@ const Home = ({
                 emptyCart={emptyCart}
             />
             
-            <ListProducts addToCart={addToCart} products={products} />
+            <ListProducts addToCart={addToCart} products={productos} />
 
             <Footer />
         </>
